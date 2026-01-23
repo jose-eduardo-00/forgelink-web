c7bb6c2553bf9af7ab534466acc698c7250a9dfe
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2026-01-23 13:02:14 -0300

    estrutura inicial das telas de autenticação

--- src/screens/auth/login/Login.jsx
+++ src/screens/auth/login/Login.jsx
@@ -1,8 +1,17 @@
+import BackgroundImage from "../../../assets/loginImg.jpg";
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login Screen</h1>
-    </div>
+    <main className="flex">
+      <div className="w-1/2 relative h-screen overflow-hidden bg-zinc-950">
+        <img
+          src={BackgroundImage}
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover opacity-70"
+        />
+      </div>
+      <div className="w-1/2"></div>
+    </main>
   );
 };
 
--- src/screens/auth/register/Register.jsx
+++ src/screens/auth/register/Register.jsx
@@ -1,8 +1,17 @@
+import BackgroundImage from "../../../assets/registerImg.jpg";
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register Screen</h1>
-    </div>
+    <main className="flex">
+      <div className="w-1/2 relative h-screen overflow-hidden bg-zinc-950">
+        <img
+          src={BackgroundImage}
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover opacity-70"
+        />
+      </div>
+      <div className="w-1/2"></div>
+    </main>
   );
 };
 
